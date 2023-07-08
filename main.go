21bbbe8e7f82a8187fcc827fb173d5f39129c496
kangbing <69076498+zhuoyue2@users.noreply.github.com>
2023-07-08 22:14:11 +0800

    uppdate

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 	// 访问静态文件
 	app.HandleDir("/static", "./static")
 	app.HandleDir("/assets", ".")
-	app.Get("/", query)
+	app.Get("/", home)
 	app.Get("/{ip:string}", query)
 	app.Listen(":5200")
 }
@@ -50,7 +50,7 @@ func query(ctx iris.Context) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	if ip == "[::1]" {
+	if len(ip) == 0 {
 		ip = remote_ip
 	}
 	// fmt.Println(len(ip), ip)
@@ -79,42 +79,42 @@ func query(ctx iris.Context) {
 	ctx.View("index.html")
 }
 
-// func home(ctx iris.Context) {
-// 	// 空接口列表用来存储列表型字典
-// 	var temp []interface{}
-// 	var sult map[string]interface{} = make(map[string]interface{}, 0)
-// 	ip, _, err := net.SplitHostPort(ctx.Request().RemoteAddr)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	// 指定ip
-// 	ip = "101.227.131.220"
-// 	fmt.Printf("home[%s]\n", ip)
-// 	city, err := db.FindMap(ip, "CN")
-// 	if err != nil {
-// 		ctx.JSON(map[string]string{"status": "0", "msg": "请检查ip地址是否正确"})
-// 	} else {
-// 		city["status"] = "1"
-// 		city["ip"] = ip
-// 		// layui返回的json数据需要满足code/msg/count/data,data需要返回列表格式
-// 		sult["code"] = 0
-// 		sult["msg"] = ""
-// 		sult["count"] = 1
-// 		temp = append(temp, city)
-// 		sult["data"] = temp
-// 		// 保存成json数据
-// 		err := saveJSONToFile(sult, "city.json")
-// 		if err != nil {
-// 			fmt.Println(err)
-// 		}
-// 		// ctx.JSON(sult)
-// 		fmt.Println(sult)
-// 		ctx.ViewData("city", sult)
-// 	}
+func home(ctx iris.Context) {
+	// 空接口列表用来存储列表型字典
+	var temp []interface{}
+	var sult map[string]interface{} = make(map[string]interface{}, 0)
+	ip, _, err := net.SplitHostPort(ctx.Request().RemoteAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 指定ip
+	// ip = "101.227.131.220"
+	fmt.Printf("home[%s]\n", ip)
+	city, err := db.FindMap(ip, "CN")
+	if err != nil {
+		ctx.JSON(map[string]string{"status": "0", "msg": "请检查ip地址是否正确"})
+	} else {
+		city["status"] = "1"
+		city["ip"] = ip
+		// layui返回的json数据需要满足code/msg/count/data,data需要返回列表格式
+		sult["code"] = 0
+		sult["msg"] = ""
+		sult["count"] = 1
+		temp = append(temp, city)
+		sult["data"] = temp
+		// 保存成json数据
+		err := saveJSONToFile(sult, "city.json")
+		if err != nil {
+			fmt.Println(err)
+		}
+		// ctx.JSON(sult)
+		fmt.Println(sult)
+		ctx.ViewData("city", sult)
+	}
 
-// 	ctx.View("index.html")
+	ctx.View("index.html")
 
-// }
+}
 
 // 用来保存json文件到当前目录下
 func saveJSONToFile(jsonObject interface{}, filename string) error {
